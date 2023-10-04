36f866919b20bb9ef390ac55ed0d5388fcef1fa5
VarshaSharma07 <121331657+VarshaSharma07@users.noreply.github.com>
2023-10-04 12:49:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ function decimalToBinary(num) {
   //Write you code here
   return decimal.toString(2);
 }
-
-window.decimalToBinary = decimalToBinary;
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
+// window.decimalToBinary = decimalToBinary;
